d028f94e94bfc514e303a7bb34ab6d09de0ea59f
FalecidoGugu <gutigeovany@gmail.com>
2024-05-08 22:48:15 -0300

    Adicionando imagens e fundo ao tabuleiro

--- ches-game/src/app/modules/chess-board/chess-board.component.ts
+++ ches-game/src/app/modules/chess-board/chess-board.component.ts
@@ -1,19 +1,25 @@
 import { Component } from "@angular/core";
-import { Color, FENChar } from "../../chess-logic/models";
+import { Color, FENChar, pieceImagePaths } from "../../chess-logic/models";
 import { ChessBoard } from "../../chess-logic/chess-board";
-import { NgFor } from "@angular/common";
+import { NgClass, NgFor, NgIf } from "@angular/common";
 
 @Component({
   selector: "app-chess-board",
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgClass, NgIf],
   templateUrl: "./chess-board.component.html",
   styleUrl: "./chess-board.component.css",
 })
 export class ChessBoardComponent {
+  public pieceImagesPaths = pieceImagePaths;
+
   private chessBoard = new ChessBoard();
   public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
   public get playerColor(): Color {
     return this.chessBoard.playerColor;
+  }
+
+  public isSquareDark(x: number, y: number): boolean {
+    return ChessBoard.isSquareDark(x, y)
   }
 }
